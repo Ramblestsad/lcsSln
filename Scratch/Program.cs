b07ca06b3ed9e6eb19cf7d61c4050598d0279209
ramblestsad <yilinw6@gmail.com>
2024-02-11 22:47:23 +0800

    feat: Avalonia csproj

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,11 +1,6 @@
 using Scratch.Utils;
-using Serilog;
 
 // Serilog setup
 await ScratchUtil.SerilogInit();
 
-// try some code
-await Playground();
-
-static async Task Playground()
-{ }
+// playground
---
+++ Zelve/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace Zelve.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
