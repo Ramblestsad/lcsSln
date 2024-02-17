12513793abd25ddd5f223d2cbade00cf8162bb21
ramblestsad <yilinw6@gmail.com>
2024-02-17 14:03:21 +0800

    chore: Remove leetcode related

--- Scratch/leetcode/DataStructure/List.cs
+++
@@ -1,7 +0,0 @@
-namespace Scratch.leetcode.DataStructure;
-
-public class ListNode(int val = 0, ListNode? next = null)
-{
-    public int val = val;
-    public ListNode? next = next ?? new ListNode();
-}
