c243402c484abc8cd9b4202c30446d1ede7a73d2
ramblestsad <yilinw6@gmail.com>
2024-03-09 23:07:49 +0800

    feat: VSCode LeetCode skeleton

---
+++ Scratch/leetcode/ListAlgorithm/easy21_mergeTwoSortedLists.cs
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=csharp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+using Algorithm.DataStructure;
+
+// @lc code=start
+public class Solution
+{
+    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+    {
+        var dummy = new ListNode(-1);
+        var p = dummy;
+        ListNode? p1 = list1, p2 = list2;
+
+        while (p1 != null && p2 != null)
+        {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
+            if (p1.val > p2.val)
+            {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            else
+            {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+
+        if (p1 != null) p.next = p1;
+        if (p2 != null) p.next = p2;
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
