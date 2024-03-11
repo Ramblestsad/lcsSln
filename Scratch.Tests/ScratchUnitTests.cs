b31ee8eb6fdc331caa4613fce6ce32c7ead8d2c9
ramblestsad <yilinw6@gmail.com>
2024-03-11 20:39:25 +0800

    feat: Add Moq to Scratch.Tests.csproj

---
+++ Scratch.Tests/ScratchUnitTests.cs
@@ -0,0 +1,43 @@
+namespace Scratch.Tests;
+[Trait("Scratch", "Unit")]
+public class ScratchUnitTests : IDisposable
+{
+    public ScratchUnitTests()
+    {
+        throw new NotImplementedException();
+    }
+    [Fact]
+    [Trait("Scratch", "Dummy")]
+    public void AbsTest()
+    {
+        Assert.Equal(4, Math.Abs(-4));
+    }
+
+    [Fact]
+    [Trait("Scratch", "Dummy")]
+    public void AbsFailTest()
+    {
+        Assert.NotEqual(4, Math.Abs(-5));
+    }
+
+    [Theory]
+    [Trait("Scratch", "Dummy")]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(7)]
+    public void IsEvenTheory(int number)
+    {
+        Assert.True(IsEven(number));
+    }
+
+    bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    public void Dispose()
+    {
+        // Do some clean-up for every test
+        throw new NotImplementedException();
+    }
+}
