2b2813723885f189af7ac0156d08b5343e2f4ca3
ramblestsad <yilinw6@gmail.com>
2024-05-11 20:18:40 +0800

    build: Up deps

--- Todo.WebApi/Program.cs
+++ Todo.WebApi/Program.cs
@@ -151,12 +151,10 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    // app.UseDeveloperExceptionPage(); // enabled by default on .net6
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        // options.RoutePrefix = string.Empty;
         options.RoutePrefix = string.Empty;
     });
 }
