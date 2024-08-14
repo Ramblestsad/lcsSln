0c1b5ee7d7ab5512d4be71f36b193e8bf3f72875
ramblestsad <yilinw6@gmail.com>
2024-08-14 00:45:03 -0400

    chore: remove lines

--- Scratch/leetcode/ListAlgo/leetcode/editor/en/[23]MergeKSortedLists.cs
+++ Scratch/leetcode/ListAlgo/leetcode/editor/en/[23]MergeKSortedLists.cs
@@ -3,17 +3,6 @@ using Algorithm.DataStructure;
 namespace Rider.Leetcode.ListAlgorithm;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
 public partial class Solution {
     public ListNode? MergeKLists(ListNode?[] lists)
     {
