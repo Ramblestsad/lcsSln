730ae689ff02c5944fdb8748125cc05d3cb969ea
ramblestsad <yilinw6@gmail.com>
2024-08-26 11:13:39 -0400

    chore: Rider lc setup

---
+++ Scratch/leetcode/ListAlgo/leetcode/editor/en/[21]MergeTwoSortedLists.cs
@@ -0,0 +1,12 @@
+using Algorithm.DataStructure;
+
+namespace Rider.Leetcode.ListAlgorithm;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public partial class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
