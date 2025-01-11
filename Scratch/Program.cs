6a64c2a8f8550d432afdb082d0ce5a8f9443995a
ramblestsad <yilinw6@gmail.com>
2025-01-11 16:03:32 -0500

    chore: change console to top-level style

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -1,11 +1,5 @@
 using Scratch.Utils;
 using Serilog;
 
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        ScratchUtils.SerilogInit();
-        Log.Information("Entry point");
-    }
-}
+ScratchUtils.SerilogInit();
+Log.Information("Entry point");
