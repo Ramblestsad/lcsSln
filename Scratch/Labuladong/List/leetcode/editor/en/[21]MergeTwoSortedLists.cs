265015562b85169ead4df2110ea288c19dc4696a
ramblestsad <yilinw6@gmail.com>
2025-06-27 15:49:11 -0400

    fix: Rollback

---
+++ Scratch/Labuladong/List/leetcode/editor/en/[21]MergeTwoSortedLists.cs
@@ -0,0 +1,10 @@
+namespace Scratch.Labuladong.Algorithms;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public partial class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- Scratch/Labuladong/Solution.cs
+++ Scratch/Labuladong/Solution.cs
@@ -10,4 +10,3 @@ public class ListNode
         this.next = next;
     }
 }
-
