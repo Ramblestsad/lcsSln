575434ce90baedb92638d0f186fde30a812e5887
ramblestsad <yilinw6@gmail.com>
2025-06-30 18:40:56 -0400

    feat: Add AVL tree impl

--- Scratch/DataStructure/Tree.cs
+++ Scratch/DataStructure/Tree.cs
@@ -1,5 +1,4 @@
 namespace Scratch.DataStructure;
-
 /// <summary>
 /// Binary tree node
 /// </summary>
@@ -111,14 +110,14 @@ public class BinarySearchTree
         // children = 2
         else
         {
-            // get the next node in the inorder traversal
+            // get the next node in the inorder traversal (右子树的最小节点）
             TreeNode? tmp = cur.Right;
             while (tmp.Left != null)
             {
                 tmp = tmp.Left;
             }
 
-            // delete tmp recursively
+            // delete tmp recursively （先删除、再覆盖原待删除节点的值）
             Remove(tmp.Val!.Value);
             cur.Val = tmp.Val;
         }
@@ -131,32 +130,199 @@ public class BinarySearchTree
 /// for insert, delete, and search operations by keeping the height difference
 /// between the left and right subtrees of every node to at most one.
 /// </summary>
-public class AvlTreeNode(int? x)
+public class AvlTreeNode(int x)
 {
-    public int? val = x;
+    public int val = x;
     private int _height;
     public AvlTreeNode? Left;
     public AvlTreeNode? Right;
 
-    private static int Height(AvlTreeNode? node)
+    public static int Height(AvlTreeNode? node)
     {
         // 空节点高度为 -1 ，叶节点高度为 0
         return node?._height ?? -1;
     }
 
-    private static void UpdateHeight(AvlTreeNode node)
+    public static void UpdateHeight(AvlTreeNode node)
     {
         // 节点高度等于最高子树高度 + 1
         node._height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
     }
 
-    private static int BalanceFactor(AvlTreeNode? node)
+    public static int BalanceFactor(AvlTreeNode? node)
     {
         if (node == null) return 0;
         // 节点平衡因子 = 左子树高度 - 右子树高度
         return Height(node.Left) - Height(node.Right);
         // tip: 设平衡因子为f，则一棵 AVL 树的任意节点的平衡因子皆满足 -1<=f<=1。
         //      就是说，AVL 树要求对每个节点，左右子树的高度差 最多为 1。
+    }
+}
+
+public class AvlTree
+{
+    public AvlTreeNode? root { get; private set; }
+
+    private static AvlTreeNode? RightRotate(AvlTreeNode? node)
+    {
+        // 以child为核心！！！
+        var child = node?.Left;
+        if (child == null || node == null) return child;
+        // `if` child has a right child, attach to node's left
+        var grandChild = child.Right;
+
+        // 以child为原点右旋
+        child.Right = node;
+        node.Left = grandChild;
+
+        // update height for node and child
+        AvlTreeNode.UpdateHeight(node);
+        AvlTreeNode.UpdateHeight(child);
+
+        return child; // return new "balanced" root for the parent node to connect
+    }
+
+    private static AvlTreeNode? LeftRotate(AvlTreeNode? node)
+    {
+        var child = node?.Right;
+        if (child == null || node == null) return child;
+        // `if` child has a left child, attach to node's right
+        var grandChild = child.Left;
+
+        child.Left = node;
+        node.Right = grandChild;
+
+        AvlTreeNode.UpdateHeight(node);
+        AvlTreeNode.UpdateHeight(child);
+
+        return child;
+    }
+
+    private static AvlTreeNode? Rotate(AvlTreeNode? node)
+    {
+        // 失衡节点的平衡因子	子节点的平衡因子	应采用的旋转方法
+        // > 1（左偏树）      >= 0             右旋
+        // > 1（左偏树）      < 0              先左旋后右旋
+        // < -1（右偏树）     <= 0             左旋
+        // < -1（右偏树）     > 0              先右旋后左旋
+        // 主要判断平衡因子的正负号来判断所采用的旋转方法
+
+        // 获取节点 node 的平衡因子
+        var balanceFactorInt = AvlTreeNode.BalanceFactor(node);
+        // 左偏树
+        if (balanceFactorInt > 1)
+        {
+            if (AvlTreeNode.BalanceFactor(node?.Left) >= 0)
+            {
+                // 右旋
+                return RightRotate(node);
+            }
+            else
+            {
+                // 先左旋后右旋
+                node!.Left = LeftRotate(node.Left);
+                return RightRotate(node);
+            }
+        }
+
+        // 右偏树
+        if (balanceFactorInt < -1)
+        {
+            if (AvlTreeNode.BalanceFactor(node?.Right) <= 0)
+            {
+                // 左旋
+                return LeftRotate(node);
+            }
+            else
+            {
+                // 先右旋后左旋
+                node!.Right = RightRotate(node.Right);
+                return LeftRotate(node);
+            }
+        }
+
+        // 平衡树，无须旋转，直接返回
+        return node;
+    }
+
+    public void Insert(int num)
+    {
+        root = InsertHelper(root, num);
+    }
+
+    private static AvlTreeNode? InsertHelper(AvlTreeNode? node, int num)
+    {
+        /*
+         * AVL 树的节点插入操作与二叉搜索树在主体上类似。
+         * 唯一的区别在于，在 AVL 树中插入节点后，从该节点到根节点的路径上可能会出现一系列失衡节点。
+         * 因此，需要从这个节点开始，自底向上执行旋转操作，使所有失衡节点恢复平衡。
+         */
+        if (node == null) return new AvlTreeNode(num);
+
+        /* 1. 查找插入位置并插入节点 */
+        if (num < node.val)
+            node.Left = InsertHelper(node.Left, num);
+        else if (num > node.val)
+            node.Right = InsertHelper(node.Right, num);
+        else
+            return node; // 重复节点不插入，直接返回
+
+        AvlTreeNode.UpdateHeight(node); // 更新节点高度
+
+        /* 2. 执行旋转操作，使该子树重新恢复平衡 */
+        node = Rotate(node);
+
+        return node;
+    }
+
+    public void Remove(int num)
+    {
+        root = RemoveHelper(root, num);
+    }
+
+    private static AvlTreeNode? RemoveHelper(AvlTreeNode? node, int num)
+    {
+        /*
+         * 类似地，在二叉搜索树的删除节点方法的基础上，需要从底至顶执行旋转操作，使所有失衡节点恢复平衡。
+         */
+        if (node == null) return null;
+        /* 1. 查找节点并删除 */
+        if (num < node.val)
+            node.Left = RemoveHelper(node.Left, num);
+        else if (num > node.val)
+            node.Right = RemoveHelper(node.Right, num);
+        else
+        {
+            if (node.Left == null || node.Right == null)
+            {
+                AvlTreeNode? child = node.Left ?? node.Right;
+                // 子节点数量 = 0 ，直接删除 node 并返回
+                if (child == null)
+                    return null;
+                // 子节点数量 = 1 ，直接删除 node
+                else
+                    node = child;
+            }
+            else
+            {
+                // 子节点数量 = 2 ，则将中序遍历的下个节点(右子树的最小节点)删除，并用该节点替换当前节点
+                AvlTreeNode? temp = node.Right;
+                while (temp.Left != null)
+                {
+                    temp = temp.Left;
+                }
+
+                node.Right = RemoveHelper(node.Right, temp.val);
+                node.val = temp.val;
+            }
+        }
+
+        AvlTreeNode.UpdateHeight(node); // 更新节点高度
+
+        /* 2. 执行旋转操作，使该子树重新恢复平衡 */
+        node = Rotate(node);
+
+        return node;
     }
 }
 
