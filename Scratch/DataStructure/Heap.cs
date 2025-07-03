a0afa70a54e49e43f9e21524b949b40dd469449f
ramblestsad <yilinw6@gmail.com>
2025-07-03 16:24:43 -0400

    feat: add top k impl

--- Scratch/DataStructure/Heap.cs
+++ Scratch/DataStructure/Heap.cs
@@ -113,3 +113,24 @@ public class MaxHeap {
         }
     }
 }
+
+public static class TopKHeap {
+
+    public static PriorityQueue<int, int> TopK(List<int> nums, int k) {
+        // 初始化小顶堆
+        PriorityQueue<int, int> heap = new();
+        // 将数组的前 k 个元素入堆
+        for (var i = 0; i < k; i++) {
+            heap.Enqueue(nums[i], nums[i]);
+        }
+        // 从第 k+1 个元素开始，保持堆的长度为 k
+        for (var i = k; i < nums.Count; i++) {
+            // 若当前元素大于堆顶元素，则将堆顶元素出堆、当前元素入堆
+            if (nums[i] > heap.Peek()) {
+                heap.Dequeue();
+                heap.Enqueue(nums[i], nums[i]);
+            }
+        }
+        return heap;
+    }
+}
