9ceb0122791b947d89cc16addfb96a7d75665824
ramblestsad <yilinw6@gmail.com>
2025-07-15 19:40:41 -0400

    feat: impl merge sort

--- Scratch/Algorithms/Sort.cs
+++ Scratch/Algorithms/Sort.cs
@@ -99,36 +99,6 @@ public static class Sort {
         }
     }
 
-    static int Partition(int[] nums, int left, int right) {
-        var pivot = nums[left];
-        var i = left + 1;
-        var j = right;
-        while (i <= j) {
-            // 移动到第一个大于pivot的数
-            while (i <= j && nums[i] <= pivot) {
-                i++;
-            }
-
-            // 移动到第一个小于pivot的数
-            while (j >= i && nums[j] >= pivot) {
-                j--;
-            }
-
-            // 已经交错，说明本身有序
-            if (i >= j) {
-                break;
-            }
-
-            // 将第一个大于pivot的数和第一个小于pivot的数交换位置
-            // 交换之后进入下一轮迭代，持续交换直到边界点
-            ( nums[i], nums[j] ) = ( nums[j], nums[i] );
-        }
-
-        // pivot与边界线交换
-        ( nums[left], nums[j] ) = ( nums[j], nums[left] );
-        return j;
-    }
-
     public static void QuickSort(int[] nums, int left, int right) {
         if (left >= right) {
             return;
@@ -167,9 +137,70 @@ public static class Sort {
         }
     }
 
-    public static void MergeSort(int[] nums, int left, int right) {
+    static int Partition(int[] nums, int left, int right) {
+        var pivot = nums[left];
+        var i = left + 1;
+        var j = right;
+        while (i <= j) {
+            // 移动到第一个大于pivot的数
+            while (i <= j && nums[i] <= pivot) {
+                i++;
+            }
+
+            // 移动到第一个小于pivot的数
+            while (j >= i && nums[j] >= pivot) {
+                j--;
+            }
+
+            // 已经交错，说明本身有序
+            if (i >= j) {
+                break;
+            }
+
+            // 将第一个大于pivot的数和第一个小于pivot的数交换位置
+            // 交换之后进入下一轮迭代，持续交换直到边界点
+            ( nums[i], nums[j] ) = ( nums[j], nums[i] );
+        }
+
+        // pivot与边界线交换
+        ( nums[left], nums[j] ) = ( nums[j], nums[left] );
+        return j;
     }
 
-    public static void Merge(int[] nums, int left, int mid, int right) {
+    public static void MergeSort(int[] nums, int left, int right) {
+        // 终止条件：当子数组长度为 1 时终止递归
+        if (left >= right) return;
+        // 划分阶段
+        var mid = left + (right - left) / 2;   // 计算中点（防止overflow)
+        MergeSort(nums, left, mid);      // 递归左子数组
+        MergeSort(nums, mid + 1, right); // 递归右子数组
+        // 合并阶段
+        Merge(nums, left, mid, right);
+    }
+
+    static void Merge(int[] nums, int left, int mid, int right) {
+        // 左子数组区间为 [left, mid], 右子数组区间为 [mid+1, right]
+        // 创建一个临时数组 tmp ，用于存放合并后的结果
+        var tmp = new int[right - left + 1];
+        // 初始化左子数组和右子数组的起始索引
+        int i = left, j = mid + 1, k = 0;
+        // 当左右子数组都还有元素时，进行比较并将较小的元素复制到临时数组中
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j])
+                tmp[k++] = nums[i++];
+            else
+                tmp[k++] = nums[j++];
+        }
+        // 将左子数组和右子数组的剩余元素复制到临时数组中
+        while (i <= mid) {
+            tmp[k++] = nums[i++];
+        }
+        while (j <= right) {
+            tmp[k++] = nums[j++];
+        }
+        // 将临时数组 tmp 中的元素复制回原数组 nums 的对应区间
+        for (k = 0; k < tmp.Length; ++k) {
+            nums[left + k] = tmp[k];
+        }
     }
 }
