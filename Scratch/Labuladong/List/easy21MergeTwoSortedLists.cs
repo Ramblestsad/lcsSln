9ac365b8be1d5922bc76eb5694c38bc4349a85f1
ramblestsad <yilinw6@gmail.com>
2025-07-22 12:34:33 -0400

    chore: migrate to vsc lc

--- Scratch/Labuladong/List/easy21MergeTwoSortedLists.cs
+++ Scratch/Labuladong/List/easy21MergeTwoSortedLists.cs
@@ -5,41 +5,26 @@
  * [21] Merge Two Sorted Lists
  */
 
-use crate::labuladong::*;
+namespace Scratch.Labuladong.Algorithms;
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
-public class Solution {
+public partial class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        
+        throw new NotImplementedException();
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,4]\n[1,3,4]\n
-// @lcpr case=end
+@lcpr case=start
+[1,2,4]\n[1,3,4]\n
+@lcpr case=end
 
-// @lcpr case=start
-// []\n[]\n
-// @lcpr case=end
+@lcpr case=start
+[]\n[]\n
+@lcpr case=end
 
-// @lcpr case=start
-// []\n[0]\n
-// @lcpr case=end
-
+@lcpr case=start
+[]\n[0]\n
+@lcpr case=end
  */
-
--- Scratch/Labuladong/List/leetcode/editor/en/[21]MergeTwoSortedLists.cs
+++
@@ -1,9 +0,0 @@
-namespace Scratch.Labuladong.Algorithms;
-
-//leetcode submit region begin(Prohibit modification and deletion)
-public partial class Solution {
-    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        throw new NotImplementedException();
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
