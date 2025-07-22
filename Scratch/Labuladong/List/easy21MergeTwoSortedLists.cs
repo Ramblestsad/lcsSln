931a4fd1e073ac8a7891c9cc710b5a1b396e07c0
ramblestsad <yilinw6@gmail.com>
2025-07-22 12:34:09 -0400

    chore: migrate to vsc lc

---
+++ Scratch/Labuladong/List/easy21MergeTwoSortedLists.cs
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=21 lang=csharp
+ * @lcpr version=30201
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+use crate::labuladong::*;
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
