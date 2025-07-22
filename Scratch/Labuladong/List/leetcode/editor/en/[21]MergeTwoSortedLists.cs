4289d04059616e19cc23270cda7885081d1c242d
ramblestsad <yilinw6@gmail.com>
2025-07-22 12:44:00 -0400

    fix: migrate back to rider

---
+++ Scratch/Labuladong/List/leetcode/editor/en/[21]MergeTwoSortedLists.cs
@@ -0,0 +1,44 @@
+namespace Scratch.Labuladong.Algorithms;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public partial class Solution {
+    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2) {
+        // Create a dummy node to simplify the merge process
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        // Traverse both lists and merge them
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // If there are remaining nodes in either list, append them
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+
+        // Return the merged list, which starts from the next of the dummy node
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
