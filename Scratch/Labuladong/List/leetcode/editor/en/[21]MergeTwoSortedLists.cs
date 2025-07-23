dbeb72fe3a9ff63a20fc0f8d62e825fc0cb887c4
ramblestsad <yilinw6@gmail.com>
2025-07-22 20:43:18 -0400

    feat: leetcode e21

--- Scratch/Labuladong/List/leetcode/editor/en/[21]MergeTwoSortedLists.cs
+++ Scratch/Labuladong/List/leetcode/editor/en/[21]MergeTwoSortedLists.cs
@@ -14,12 +14,36 @@ namespace Scratch.Labuladong.Algorithms;
  */
 public partial class Solution {
     public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2) {
-        // dummy is a placeholder node that points to the head of the merged list
-        // current points the current merging position
+        // dummy: a placeholder node that points to the head of the merged list
         var dummy = new ListNode();
+        // current points the current merging position
         var current = dummy;
 
-        throw new NotImplementedException();
+        // traver two lists until one of them exhausted
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                // append smaller node to merging node
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            // move current pointer
+            current = current.next;
+        }
+
+        // traverse the remaining nodes for one of the lists
+        if (list1 != null) {
+            current.next = list1;
+        }
+
+        if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
