4c4c3b790002c538c015c9e8706440c287511f2a
ramblestsad <yilinw6@gmail.com>
2025-07-25 12:36:32 -0400

    feat: lc m86 skeleton

---
+++ Scratch/Labuladong/List/leetcode/editor/en/[86]PartitionList.cs
@@ -0,0 +1,20 @@
+namespace Scratch.Labuladong.Algorithms;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public partial class Solution {
+    public ListNode? Partition(ListNode? head, int x) {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
