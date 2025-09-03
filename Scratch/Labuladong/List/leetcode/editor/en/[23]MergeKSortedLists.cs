12f14217389a3454c65963ed4806098dc163bb0e
ramblestsad <yilinw6@gmail.com>
2025-09-03 11:51:17 -0400

    build: Upgrade deps

---
+++ Scratch/Labuladong/List/leetcode/editor/en/[23]MergeKSortedLists.cs
@@ -0,0 +1,20 @@
+namespace Scratch.Labuladong.Algorithms;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public partial class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
