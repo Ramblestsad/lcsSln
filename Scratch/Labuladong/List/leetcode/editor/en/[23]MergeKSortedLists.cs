d835623e3b24bdacca519fcbf6c2addaee918646
ramblestsad <yilinw6@gmail.com>
2025-09-27 16:45:40 -0400

    feat: lc 23

--- Scratch/Labuladong/List/leetcode/editor/en/[23]MergeKSortedLists.cs
+++ Scratch/Labuladong/List/leetcode/editor/en/[23]MergeKSortedLists.cs
@@ -13,8 +13,35 @@ namespace Scratch.Labuladong.Algorithms;
  * }
  */
 public partial class Solution {
-    public ListNode MergeKLists(ListNode[] lists) {
-        throw new NotImplementedException();
+    public ListNode? MergeKLists(ListNode?[] lists) {
+        if (lists.Length == 0) {
+            return null!;
+        }
+
+        var dummy = new ListNode();
+        var p = dummy;
+        // construct minHeap
+        var pq = new PriorityQueue<ListNode, int>();
+        // push heads of k lists to pa
+        foreach (var node in lists) {
+            if (node is not null) {
+                pq.Enqueue(node, node.val);
+            }
+        }
+
+        while (pq.Count > 0) {
+            var minNode = pq.Dequeue();
+            p.next = minNode;
+            if (minNode.next != null) pq.Enqueue(minNode.next, minNode.next.val);
+            p = p.next;
+        }
+
+        return dummy.next;
     }
+
+    /* time complexity:
+     * The most number elements in pa is k, time complexity for every Enqueue or Dequeue is O(logk)
+     * Every nodes in k lists will be enqueued and dequeued, so O(Nlogk)
+     */
 }
 //leetcode submit region end(Prohibit modification and deletion)
