fe376fbd39bcdf6c60d5c54c1fceb5577d9852a8
ramblestsad <yilinw6@gmail.com>
2025-10-06 16:21:00 -0400

    feat: lc 876

---
+++ Scratch/Labuladong/List/leetcode/editor/en/[142]LinkedListCycleII.cs
@@ -0,0 +1,24 @@
+namespace Scratch.Labuladong.Algorithms;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public partial class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        throw new NotImplementedException();
+    }
+
+    private static bool HasCycle(ListNode head) {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- Scratch/Labuladong/List/leetcode/editor/en/[876]MiddleOfTheLinkedList.cs
+++ Scratch/Labuladong/List/leetcode/editor/en/[876]MiddleOfTheLinkedList.cs
@@ -14,7 +14,20 @@ namespace Scratch.Labuladong.Algorithms;
  */
 public partial class Solution {
     public ListNode MiddleNode(ListNode head) {
-        throw new NotImplementedException();
+        // 每当慢指针 slow 前进一步，快指针 fast 就前进两步，这样，当 fast 走到链表末尾时，slow 就指向了链表中点。
+        var slow = head;
+        var fast = head;
+
+        while (fast != null && fast.next != null) {
+            // Note:
+            // 这里需要同时判断 fast != null 和 fast.next != null
+            // 如果长度为偶数，fast.next 可能会是 null，此时再访问 fast.next.next 就会抛出异常
+            // 如果长度为奇数，fast正好为最后一个节点时，会进入循环，slow会多走一步。
+            slow = slow?.next;
+            fast = fast.next?.next;
+        }
+
+        return slow!;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
