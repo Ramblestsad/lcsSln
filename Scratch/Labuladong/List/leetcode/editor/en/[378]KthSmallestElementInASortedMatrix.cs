f3c612f55a4437e2a071e2dc097f5763fe1749cf
ramblestsad <yilinw6@gmail.com>
2025-10-07 14:31:07 -0400

    feat: lc 378

--- Scratch/Labuladong/List/leetcode/editor/en/[378]KthSmallestElementInASortedMatrix.cs
+++ Scratch/Labuladong/List/leetcode/editor/en/[378]KthSmallestElementInASortedMatrix.cs
@@ -3,7 +3,27 @@ namespace Scratch.Labuladong.Algorithms.KthSmallestElementInASortedMatrix;
 //leetcode submit region begin(Prohibit modification and deletion)
 public class Solution {
     public int KthSmallest(int[][] matrix, int k) {
-        throw new NotImplementedException();
+        // 当成合并K个有序链表，用PriorityQueue
+        var pq = new PriorityQueue<(int, int), int>();
+        // 每行第一个元素的i, j index入堆，比较其值
+        for (int i = 0; i < matrix.Length; i++) {
+            pq.Enqueue((i, 0), matrix[i][0]);
+        }
+
+        var res = -1;
+
+        while (pq.Count > 0 && k > 0) {
+            var (i, j) = pq.Dequeue();
+            res = matrix[i][j];
+
+            k--;
+
+            if (j + 1 < matrix[i].Length) {
+                pq.Enqueue((i, j + 1), matrix[i][j + 1]);
+            }
+        }
+
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
