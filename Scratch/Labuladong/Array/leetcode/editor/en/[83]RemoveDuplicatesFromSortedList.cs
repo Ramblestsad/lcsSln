1a5452a7bd0c2a1d68cf6b95df6cfa8ac34d4eda
ramblestsad <yilinw6@gmail.com>
2025-10-24 18:08:33 -0400

    fix: update lc 26 comment

---
+++ Scratch/Labuladong/Array/leetcode/editor/en/[83]RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,21 @@
+namespace Scratch.Labuladong.Algorithms.RemoveDuplicatesFromSortedList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
