a89e2a5acce99c40c08760c6430f92eb5b7e039a
ramblestsad <yilinw6@gmail.com>
2025-10-25 17:36:09 -0400

    leetcode: 18 recursive nSum

--- Scratch/Labuladong/Array/leetcode/editor/en/[18]FourSum.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[18]FourSum.cs
@@ -6,26 +6,29 @@ public class Solution
     public IList<IList<int>> FourSum(int[] nums, int target)
     {
         Array.Sort(nums);
-        var n = nums.Length;
-        var res = new List<IList<int>>();
+        // var n = nums.Length;
+        // var res = new List<IList<int>>();
+        //
+        // // 穷举 fourSum 的第一个数
+        // for (var i = 0; i < n; i++)
+        // {
+        //     // 对 target - nums[i] 计算 threeSum
+        //     var triples = ThreeSumTarget(nums, i + 1, target - nums[i]);
+        //     // 如果存在满足条件的三元组，再加上 nums[i] 就是结果四元组
+        //     foreach (var triple in triples)
+        //     {
+        //         triple.Add(nums[i]);
+        //         res.Add(triple);
+        //     }
+        //
+        //     // fourSum 的第一个数不能重复
+        //     while (i < n - 1 && nums[i] == nums[i + 1]) i++;
+        // }
+        //
+        // return res;
 
-        // 穷举 fourSum 的第一个数
-        for (var i = 0; i < n; i++)
-        {
-            // 对 target - nums[i] 计算 threeSum
-            var triples = ThreeSumTarget(nums, i + 1, target - nums[i]);
-            // 如果存在满足条件的三元组，再加上 nums[i] 就是结果四元组
-            foreach (var triple in triples)
-            {
-                triple.Add(nums[i]);
-                res.Add(triple);
-            }
-
-            // fourSum 的第一个数不能重复
-            while (i < n - 1 && nums[i] == nums[i + 1]) i++;
-        }
-
-        return res;
+        // n 为 4，从 nums[0] 开始计算和为 target 的四元组
+        return NSumTarget(nums, 4, 0, target);
     }
 
     private List<IList<int>> ThreeSumTarget(int[] nums, int start, long target)
@@ -52,10 +55,10 @@ public class Solution
         return res;
     }
 
-    private List<List<int>> TwoSumTarget(int[] nums, int start, long target)
+    private List<IList<int>> TwoSumTarget(int[] nums, int start, long target)
     {
         // 包list为了动态添加元素
-        var res = new List<List<int>>();
+        var res = new List<IList<int>>();
         var left = start;
         var right = nums.Length - 1;
 
@@ -80,6 +83,59 @@ public class Solution
             if (sum > target)
             {
                 while (left < right && nums[right] == rVal) right--;
+            }
+        }
+
+        return res;
+    }
+
+    private List<IList<int>> NSumTarget(int[] nums, int n, int start, long target)
+    {
+        var sz = nums.Length;
+        var res = new List<IList<int>>();
+        // 至少是 2Sum，且数组大小不应该小于 n
+        if (n < 2 || sz < n) return res;
+
+        // 2Sum 是 base case
+        if (n == 2)
+        {
+            // 双指针那一套操作
+            var lo = start;
+            var hi = sz - 1;
+            while (lo < hi)
+            {
+                var sum = nums[lo] + nums[hi];
+                int left = nums[lo], right = nums[hi];
+                if (sum < target)
+                {
+                    while (lo < hi && nums[lo] == left) lo++;
+                }
+                else if (sum > target)
+                {
+                    while (lo < hi && nums[hi] == right) hi--;
+                }
+                else
+                {
+                    res.Add([left, right]);
+                    while (lo < hi && nums[lo] == left) lo++;
+                    while (lo < hi && nums[hi] == right) hi--;
+                }
+            }
+        }
+        else
+        {
+            // n > 2 时，递归计算 (n-1)Sum 的结果
+            for (var i = start; i < sz; i++)
+            {
+                var sub = NSumTarget(nums, n - 1, i + 1, target - nums[i]);
+                foreach (var s in sub)
+                {
+                    // (n-1)Sum 加上 nums[i] 就是 nSum
+                    s.Add(nums[i]);
+                    res.Add(s);
+                }
+
+                while (i < sz - 1 && nums[i] == nums[i + 1]) i++;
             }
         }
 
