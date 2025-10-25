59dfb77d0d0daa50124c809ef6fbd118798d86f3
ramblestsad <yilinw6@gmail.com>
2025-10-25 16:16:07 -0400

    leetcode: 167

---
+++ Scratch/Labuladong/Array/leetcode/editor/en/[167]TwoSumIIInputArrayIsSorted.cs
@@ -0,0 +1,23 @@
+namespace Scratch.Labuladong.Algorithms.TwoSumIIInputArrayIsSorted;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var left = 0;
+        var right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            var sum = numbers[left] + numbers[right];
+            if (sum == target) return [left + 1, right + 1];
+            // 记住numbers是非递减数组，移动左指针，sum就会变大；反之移动右指针，sum就会变小;
+            if (sum < target) left++;
+            if (sum > target) right--;
+
+        }
+
+        return [-1, -1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
