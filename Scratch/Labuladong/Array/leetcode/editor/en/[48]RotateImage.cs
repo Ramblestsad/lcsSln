519c1319d345e177ef5e4a396e8646edfc8f79be
ramblestsad <yilinw6@gmail.com>
2025-11-15 14:04:03 -0500

    leetcode: 48 add reverse rotate

--- Scratch/Labuladong/Array/leetcode/editor/en/[48]RotateImage.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[48]RotateImage.cs
@@ -23,10 +23,25 @@ public class Solution
         }
     }
 
+    public void Rotate2(int[][] matrix)
+    {
+        var n = matrix.Length;
+        // 沿左下到右上的对角线镜像对称二维矩阵
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n - i; j++)
+            {
+                // swap(matrix[i][j], matrix[n-j-1][n-i-1])
+                ( matrix[i][j], matrix[n-j-1][n-i-1] ) = ( matrix[n-j-1][n-i-1], matrix[i][j] );
+            }
+        }
+    }
+
     private void reverse(int[] arr)
     {
         int i = 0, j = arr.Length - 1;
-        while (j > i) {
+        while (j > i)
+        {
             // swap(arr[i], arr[j]);
             ( arr[i], arr[j] ) = ( arr[j], arr[i] );
             i++;
