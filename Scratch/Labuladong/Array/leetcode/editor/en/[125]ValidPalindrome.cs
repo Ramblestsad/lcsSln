c5c710ed580abb50bc0e1bc119e429497249b796
ramblestsad <yilinw6@gmail.com>
2025-11-26 22:29:59 -0500

    leetcode: 125

--- Scratch/Labuladong/Array/leetcode/editor/en/[125]ValidPalindrome.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[125]ValidPalindrome.cs
@@ -1,11 +1,39 @@
+
+
 namespace Scratch.Labuladong.Algorithms.ValidPalindrome;
 
 //leetcode submit region begin(Prohibit modification and deletion)
+using System.Text;
+
 public class Solution
 {
     public bool IsPalindrome(string s)
     {
-        return false;
+
+        var builder = new StringBuilder(s.Length);
+        foreach (var ch in s)
+        {
+            if (char.IsLetterOrDigit(ch))
+            {
+                builder.Append(char.ToLowerInvariant(ch));
+            }
+        }
+
+        s = builder.ToString();
+
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
