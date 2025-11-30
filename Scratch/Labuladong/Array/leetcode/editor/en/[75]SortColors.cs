ae7c8dcb8806e2f7e9bcb7c9259cc643a2ca5f92
ramblestsad <yilinw6@gmail.com>
2025-11-30 16:55:17 -0500

    leetcode: 75 optimize

--- Scratch/Labuladong/Array/leetcode/editor/en/[75]SortColors.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[75]SortColors.cs
@@ -5,31 +5,26 @@ public class Solution
 {
     public void SortColors(int[] nums)
     {
-        int currentIndex = 0, zeros = 0, ones = 0, twos = 0;
+        // 注意区间的开闭，初始化时区间内应该没有元素
+        // 所以我们定义 [0，p0) 是元素 0 的区间，(p2, nums.length - 1] 是 2 的区间
+        int p0 = 0, p2 = nums.Length - 1, p = 0;
+        // 由于 p2 是开区间，所以 p <= p2
+        while (p <= p2) {
+            if (nums[p] == 0)
+            {
+                ( nums[p0], nums[p] ) = ( nums[p], nums[p0] );
+                p0++;
+            } else if (nums[p] == 2) {
+                ( nums[p2], nums[p] ) = ( nums[p], nums[p2] );
+                p2--;
+            } else if (nums[p] == 1) {
+                p++;
+            }
 
-        foreach (var num in nums)
-        {
-            if (num == 0) zeros++;
-            if (num == 1) ones++;
-            if (num == 2) twos++;
-        }
-
-        for (; zeros > 0; zeros--)
-        {
-            nums[currentIndex] = 0;
-            currentIndex++;
-        }
-
-        for (; ones > 0; ones--)
-        {
-            nums[currentIndex] = 1;
-            currentIndex++;
-        }
-
-        for (; twos > 0; twos--)
-        {
-            nums[currentIndex] = 2;
-            currentIndex++;
+            // 因为小于 p0 都是 0，所以 p 不要小于 p0
+            if (p < p0) {
+                p = p0;
+            }
         }
     }
 }
