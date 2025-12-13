ac36bd66770e7d0a8e642fcfd04fc9c14afe40cb
ramblestsad <yilinw6@gmail.com>
2025-12-13 15:54:04 -0500

    leetcode: 1329

--- Scratch/Labuladong/Array/leetcode/editor/en/[1329]SortTheMatrixDiagonally.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[1329]SortTheMatrixDiagonally.cs
@@ -5,7 +5,47 @@ public class Solution
 {
     public int[][] DiagonalSort(int[][] mat)
     {
-        throw new NotImplementedException();
+        // 如何快速判断两个元素坐标是否在同一个对角线上？
+        // 在同一个对角线上的元素，其横纵坐标之差是相同的
+        int m = mat.Length, n = mat[0].Length;
+        var diagonals = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                // 横纵坐标之差可以作为一条对角线的 ID
+                var diagonalId = i - j;
+                if (!diagonals.TryGetValue(diagonalId, out var list))
+                {
+                    list = new List<int>();
+                    diagonals[diagonalId] = list;
+                }
+
+                list.Add(mat[i][j]);
+            }
+        }
+
+        // 从数组末尾删除元素效率较高，所以把 ArrayList 倒序排序
+        foreach (var diagonal in diagonals.Values)
+        {
+            diagonal.Sort();
+            diagonal.Reverse();
+        }
+
+        // 把排序结果回填二维矩阵
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                var diagonal = diagonals[i - j];
+                var lastIndex = diagonal.Count - 1;
+                mat[i][j] = diagonal[lastIndex];
+                diagonal.RemoveAt(lastIndex);
+            }
+        }
+
+        return mat;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- Scratch/Labuladong/Array/leetcode/editor/en/[977]SquaresOfASortedArray.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[977]SquaresOfASortedArray.cs
@@ -6,8 +6,8 @@ public class Solution
     public int[] SortedSquares(int[] nums)
     {
         // 直接将双指针分别初始化在 nums 的开头和结尾，相当于合并两个从大到小排序的数组，和 88 题类似
-        var n = nums.Length;
         // 两个指针分别初始化在正负子数组绝对值最大的元素索引
+        var n = nums.Length;
         int i = 0, j = n - 1;
         // 得到的有序结果是降序的
         var p = n - 1;
