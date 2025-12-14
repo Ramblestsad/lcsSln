d418b326938e769355ad330d467f7fe69d9d7025
ramblestsad <yilinw6@gmail.com>
2025-12-13 19:08:08 -0500

    leetcode: 14 patch comments

--- Scratch/Labuladong/Array/leetcode/editor/en/[14]LongestCommonPrefix.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[14]LongestCommonPrefix.cs
@@ -5,11 +5,16 @@ public class Solution
 {
     public string LongestCommonPrefix(string[] strs)
     {
-        // 把字符串列表看成一个二维数组，然后用一个嵌套 for 循环计算这个二维数组前面有多少列的元素完全相同即可
+        // 把字符串列表看成一个二维数组，
+        // 外层循环以第一行（即 strs[0]）的字符数为最大列数 n。
+        // 对每一列 col，内层循环从第二行开始与前一行比较该列字符
         var m = strs.Length;
         // 以第一行的列数为基准
         var n = strs[0].Length;
 
+        // ["flower",
+        //  "flow",
+        //  "flight"]
         for (int col = 0; col < n; col++)
         {
             for (int row = 1; row < m; row++)
@@ -17,16 +22,18 @@ public class Solution
                 var thisStr = strs[row];
                 var prevStr = strs[row - 1];
 
-                // 判断每个字符串的 col 索引是否都相同
+                // 若某个字符串长度不够
+                // 或当前列字符不同
                 if (col >= thisStr.Length || col >= prevStr.Length ||
                     thisStr[col] != prevStr[col])
                 {
-                    // 发现不匹配的字符，只有 strs[row][0..col-1] 是公共前缀
+                    // 说明公共前缀在 0..col-1
                     return strs[row][0..col];
                 }
             }
         }
 
+        // 若所有列都匹配，则整个第一行即为答案
         return strs[0];
     }
 }
