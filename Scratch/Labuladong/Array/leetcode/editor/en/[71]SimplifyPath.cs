63d475d42609e914b7027e45952ee04a7ce600e8
ramblestsad <yilinw6@gmail.com>
2025-12-31 13:20:15 -0800

    leetcode: 71 patch comments

--- Scratch/Labuladong/Array/leetcode/editor/en/[71]SimplifyPath.cs
+++ Scratch/Labuladong/Array/leetcode/editor/en/[71]SimplifyPath.cs
@@ -8,11 +8,17 @@ public class Solution
         var parts = path.Split("/");
         var stk = new Stack<string>();
 
+        // "a/./b/..//c/
+        // parts: ["a", ".", "b", "..", "", "c", ""]
+        // “b” 与 ".." 抵消
+        // res = "/a/c
         foreach (var part in parts)
         {
             if (part.Length == 0 || part == ".") continue;
             if (part == "..")
             {
+                // ".." 碰到上级目录就跟栈里已经压入的“抵消”
+                // 栈里没有就忽略，说明还是在根目录“/”
                 if (stk.Count != 0) stk.Pop();
                 continue;
             }
