3e70eb0b7103684d7fec0a20296be793e9304bcc
ramblestsad <yilinw6@gmail.com>
2026-01-17 23:36:35 -0800

    feat: implement Minimum Depth of Binary Tree solution and update .gitignore

--- Scratch/Labuladong/Solution.cs
+++ Scratch/Labuladong/Solution.cs
@@ -82,3 +82,16 @@ public class MonotonicQueue<T> where T: IComparable<T>
         return q.Count == 0;
     }
 }
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ Scratch/Labuladong/Tree/leetcode/editor/en/[111]MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,26 @@
+namespace Scratch.Labuladong.Algorithms.MinimumDepthOfBinaryTree;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int MinDepth(TreeNode root)
+    {
+        var depth = 0;
+
+        return depth;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
