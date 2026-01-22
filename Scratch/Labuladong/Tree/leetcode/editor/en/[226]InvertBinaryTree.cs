ba180c31a5449e0b75b128c7fa99dac483c2774f
ramblestsad <yilinw6@gmail.com>
2026-01-21 20:36:17 -0800

    feat: add InvertBinaryTree solution and update Dockerfile and project references

---
+++ Scratch/Labuladong/Tree/leetcode/editor/en/[226]InvertBinaryTree.cs
@@ -0,0 +1,24 @@
+namespace Scratch.Labuladong.Algorithms.InvertBinaryTree;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
