14bcd58a585b68d4199b69bb2b01b54ddb52342d
ramblestsad <yilinw6@gmail.com>
2026-01-22 19:45:10 -0800

    leetcode: 226

--- Scratch/Labuladong/Tree/leetcode/editor/en/[226]InvertBinaryTree.cs
+++ Scratch/Labuladong/Tree/leetcode/editor/en/[226]InvertBinaryTree.cs
@@ -18,7 +18,20 @@ public class Solution
 {
     public TreeNode InvertTree(TreeNode root)
     {
+        Traverse(root);
         return root;
+    }
+
+    private void Traverse(TreeNode? root)
+    {
+        if (root == null) return;
+
+        // 前序位置
+        // 每一个节点需要做的事就是交换它的左右子节点
+        ( root.left, root.right ) = ( root.right, root.left );
+
+        Traverse(root.left);
+        Traverse(root.right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
