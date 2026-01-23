b8da25b1fcb5f1edb59442567f0ea731236d1408
ramblestsad <yilinw6@gmail.com>
2026-01-22 20:10:04 -0800

    leetcode: 114

---
+++ Scratch/Labuladong/Tree/leetcode/editor/en/[114]FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,47 @@
+namespace Scratch.Labuladong.Algorithms.FlattenBinaryTreeToLinkedList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void Flatten(TreeNode? root)
+    {
+        // base case
+        if (root == null) return;
+
+        // 把左右子树拉平
+        Flatten(root.left);
+        Flatten(root.right);
+
+        // 后序位置
+        // 1、左右子树已经被拉平成一条链表
+        var l = root.left;
+        var r = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = l;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null)
+        {
+            p = p.right;
+        }
+
+        p.right = r;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
