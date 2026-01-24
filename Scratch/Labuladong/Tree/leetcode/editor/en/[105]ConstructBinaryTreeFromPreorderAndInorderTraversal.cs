7f074bd2014cc6e85b1df68d8cd7e1a8d50c0bcc
ramblestsad <yilinw6@gmail.com>
2026-01-23 20:39:32 -0800

    leetcode: 105

--- Scratch/Labuladong/Tree/leetcode/editor/en/[105]ConstructBinaryTreeFromPreorderAndInorderTraversal.cs
+++ Scratch/Labuladong/Tree/leetcode/editor/en/[105]ConstructBinaryTreeFromPreorderAndInorderTraversal.cs
@@ -21,7 +21,39 @@ public class Solution
 
     public TreeNode? BuildTree(int[] preorder, int[] inorder)
     {
-        return new TreeNode();
+        for (int i = 0; i < inorder.Length; i++)
+        {
+            valToIndex[inorder[i]] = i;
+        }
+
+        return Build(preorder, 0, preorder.Length - 1,
+                     inorder, 0, inorder.Length - 1);
+    }
+
+    TreeNode? Build(int[] preorder, int preStart, int preEnd,
+        int[] inorder, int inStart, int inEnd)
+    {
+        if (preStart > preEnd)
+        {
+            return null;
+        }
+
+        // root 节点对应的值就是前序遍历数组的第一个元素
+        var rootVal = preorder[preStart];
+        // rootVal 在中序遍历数组中的索引
+        valToIndex.TryGetValue(rootVal, out var index);
+
+        var lefSize = index - inStart;
+
+        // 构造root
+        var root = new TreeNode(rootVal);
+        // 递归构造左右子树
+        root.left = Build(preorder, preStart + 1, preStart + lefSize,
+                          inorder, inStart, index - 1);
+        root.right = Build(preorder, preStart + lefSize + 1, preEnd,
+                           inorder, index + 1, inEnd);
+
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
