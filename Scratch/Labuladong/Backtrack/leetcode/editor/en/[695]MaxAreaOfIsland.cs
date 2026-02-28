5d99438e23fcbdc9a3259e6f6763af901832a117
ramblestsad <yilinw6@gmail.com>
2026-02-28 15:17:58 -0800

    leetcode: 695

---
+++ Scratch/Labuladong/Backtrack/leetcode/editor/en/[695]MaxAreaOfIsland.cs
@@ -0,0 +1,46 @@
+namespace Scratch.Labuladong.Algorithms.MaxAreaOfIsland;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution
+{
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+        // 记录岛屿的最大面积
+        var res = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    // 淹没岛屿，并更新最大岛屿面积
+                    res = Math.Max(res, dfs(grid, i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    // 淹没与 (i, j) 相邻的陆地，并返回淹没的陆地面积
+    int dfs(int[][] grid, int i, int j)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n) return 0;
+        if (grid[i][j] == 0) return 0;
+
+        // 将 (i, j) 变成海水
+        grid[i][j] = 0;
+
+        return dfs(grid, i + 1, j)
+               + dfs(grid, i, j + 1)
+               + dfs(grid, i - 1, j)
+               + dfs(grid, i, j - 1) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
