1d974739e33214ce15f4695582589ee4deee8f39
ramblestsad <yilinw6@gmail.com>
2026-03-07 12:51:19 -0800

    build: upgrade deps

---
+++ Scratch/Labuladong/DP/leetcode/editor/en/[931]MinimumFallingPathSum.cs
@@ -0,0 +1,53 @@
+namespace Scratch.Labuladong.Algorithms.MinimumFallingPathSum;
+
+//Given an n x n array of integers matrix, return the minimum sum of any
+//falling path through matrix.
+//
+// A falling path starts at any element in the first row and chooses the
+//element in the next row that is either directly below or diagonally left/right.
+//Specifically, the next element from position (row, col) will be (row + 1, col - 1), (
+//row + 1, col), or (row + 1, col + 1).
+//
+//
+// Example 1:
+//
+//
+//Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+//Output: 13
+//Explanation: There are two falling paths with a minimum sum as shown.
+//
+//
+// Example 2:
+//
+//
+//Input: matrix = [[-19,57],[-40,-5]]
+//Output: -59
+//Explanation: The falling path with a minimum sum is shown.
+//
+//
+//
+// Constraints:
+//
+//
+// n == matrix.length == matrix[i].length
+// 1 <= n <= 100
+// -100 <= matrix[i][j] <= 100
+//
+//
+// Related TopicsArray | Dynamic Programming | Matrix
+//
+// 👍 6975, 👎 174bug 反馈 | 使用指南 | 更多配套插件
+//
+//
+//
+//
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution
+{
+    public int MinFallingPathSum(int[][] matrix)
+    {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
