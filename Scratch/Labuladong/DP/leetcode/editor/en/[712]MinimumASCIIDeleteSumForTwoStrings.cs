235a59585d6356bef4dec2940ff2a497d306c94f
ramblestsad <yilinw6@gmail.com>
2026-03-10 17:55:28 -0700

    leetcode: 712 add table impl

--- Scratch/Labuladong/DP/leetcode/editor/en/[712]MinimumASCIIDeleteSumForTwoStrings.cs
+++ Scratch/Labuladong/DP/leetcode/editor/en/[712]MinimumASCIIDeleteSumForTwoStrings.cs
@@ -112,5 +112,43 @@ public class Solution
 
         return memo[i][j];
     }
+
+    public int MinimumDeleteSumDpTable(String s1, String s2)
+    {
+        var m = s1.Length;
+        var n = s2.Length;
+        var dp = new int[m + 1][];
+        for (int i = 0; i < m + 1; i++)
+        {
+            dp[i] = new int[n + 1];
+        }
+
+        for (int i = 1; i < m + 1; i++)
+        {
+            dp[i][0] = dp[i - 1][0] + s1[i - 1];
+        }
+
+        for (int j = 1; j < n + 1; j++)
+        {
+            dp[0][j] = dp[0][j - 1] + s2[j - 1];
+        }
+
+        for (int i = 1; i < m + 1; i++)
+        {
+            for (int j = 1; j < n + 1; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = Math.Min(dp[i - 1][j] + s1[i - 1], dp[i][j - 1] + s2[j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
