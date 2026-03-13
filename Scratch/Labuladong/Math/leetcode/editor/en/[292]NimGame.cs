26092994e5c2fc21983e0d4c03f64ab6d2eec669
ramblestsad <yilinw6@gmail.com>
2026-03-12 18:10:00 -0700

    leetcode: math one-liner

---
+++ Scratch/Labuladong/Math/leetcode/editor/en/[292]NimGame.cs
@@ -0,0 +1,69 @@
+namespace Scratch.Labuladong.Algorithms.NimGame;
+
+//You are playing the following Nim Game with your friend:
+//
+//
+// Initially, there is a heap of stones on the table.
+// You and your friend will alternate taking turns, and you go first.
+// On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+// The one who removes the last stone is the winner.
+//
+//
+// Given n, the number of stones in the heap, return true if you can win the
+// game assuming both you and your friend play optimally, otherwise return false.
+//
+//
+// Example 1:
+//
+//
+//Input: n = 4
+//Output: false
+//Explanation: These are the possible outcomes:
+//1. You remove 1 stone. Your friend removes 3 stones, including the last stone.
+// Your friend wins.
+//2. You remove 2 stones. Your friend removes 2 stones, including the last
+//stone. Your friend wins.
+//3. You remove 3 stones. Your friend removes the last stone. Your friend wins.
+//In all outcomes, your friend wins.
+//
+//
+// Example 2:
+//
+//
+//Input: n = 1
+//Output: true
+//
+//
+// Example 3:
+//
+//
+//Input: n = 2
+//Output: true
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= n <= 2³¹ - 1
+//
+//
+// Related TopicsMath | Brainteaser | Game Theory
+//
+// 👍 1998, 👎 2750bug 反馈 | 使用指南 | 更多配套插件
+//
+//
+//
+//
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution
+{
+    public bool CanWinNim(int n)
+    {
+        // 如果上来就踩到 4 的倍数，那就认输吧
+        // 否则，可以把对方控制在 4 的倍数，必胜
+        return n % 4 != 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
