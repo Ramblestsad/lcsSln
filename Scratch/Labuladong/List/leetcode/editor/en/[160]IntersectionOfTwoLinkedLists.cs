225e0fd9d33e335c92c3e4c05d9d334105cb1bcb
ramblestsad <yilinw6@gmail.com>
2026-03-17 16:53:21 -0700

    leetcode: change 162 impl

--- Scratch/Labuladong/List/leetcode/editor/en/[160]IntersectionOfTwoLinkedLists.cs
+++ Scratch/Labuladong/List/leetcode/editor/en/[160]IntersectionOfTwoLinkedLists.cs
@@ -13,47 +13,19 @@ public class Solution
 {
     public ListNode? GetIntersectionNode(ListNode headA, ListNode headB)
     {
-        int lenA = 0, lenB = 0;
-        // 首先计算两条链表长度
-        for (var p1 = headA; p1 != null; p1 = p1.next)
+        // p1 指向 A 链表头结点，p2 指向 B 链表头结点
+        ListNode? p1 = headA, p2 = headB;
+        while (p1 != p2)
         {
-            lenA++;
+            // p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            if (p1 == null) p1 = headB;
+            else p1 = p1.next;
+            // p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            if (p2 == null) p2 = headA;
+            else p2 = p2.next;
         }
 
-        for (var p2 = headB; p2 != null; p2 = p2.next)
-        {
-            lenB++;
-        }
-
-        // 抹平长度差值：让更长的链表先走差值步数
-        // 这样两条链表在之后的循环可以同时结束
-        var pa = headA;
-        var pb = headB;
-        if (lenA > lenB)
-        {
-            for (var i = 0; i < lenA - lenB; i++)
-            {
-                pa = pa?.next;
-            }
-        }
-        else
-        {
-            for (var i = 0; i < lenB - lenA; i++)
-            {
-                pb = pb?.next;
-            }
-        }
-
-        // 看两个指针是否会相同，p1 == p2 时有两种情况：
-        // 1、要么是两条链表不相交，他俩同时走到尾部空指针
-        // 2、要么是两条链表相交，他俩走到两条链表的相交点
-        while (pa != pb)
-        {
-            pa = pa?.next;
-            pb = pb?.next;
-        }
-
-        return pa;
+        return p1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
