02507c045f6480899767117d19d0e1d90dcac4f8
ramblestsad <yilinw6@gmail.com>
2026-03-30 12:08:01 -0700

    build: upgrade deps

---
+++ Scratch/Labuladong/Array/leetcode/editor/en/[1314]MatrixBlockSum.cs
@@ -0,0 +1,53 @@
+namespace Scratch.Labuladong.Algorithms.MatrixBlockSum;
+
+//Given a m x n matrix mat and an integer k, return a matrix answer where each
+//answer[i][j] is the sum of all elements mat[r][c] for:
+//
+//
+// i - k <= r <= i + k,
+// j - k <= c <= j + k, and
+// (r, c) is a valid position in the matrix.
+//
+//
+//
+// Example 1:
+//
+//
+//Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+//Output: [[12,21,16],[27,45,33],[24,39,28]]
+//
+//
+// Example 2:
+//
+//
+//Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 2
+//Output: [[45,45,45],[45,45,45],[45,45,45]]
+//
+//
+//
+// Constraints:
+//
+//
+// m == mat.length
+// n == mat[i].length
+// 1 <= m, n, k <= 100
+// 1 <= mat[i][j] <= 100
+//
+//
+// Related TopicsArray | Matrix | Prefix Sum
+//
+// 👍 2513, 👎 400bug 反馈 | 使用指南 | 更多配套插件
+//
+//
+//
+//
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution
+{
+    public int[][] MatrixBlockSum(int[][] mat, int k)
+    {
+        throw new NotImplementedException();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
