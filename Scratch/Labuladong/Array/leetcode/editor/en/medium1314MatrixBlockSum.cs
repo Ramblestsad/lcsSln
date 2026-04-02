aaab41898c59d993b46594b12d7e44185a8c7503
ramblestsad <yilinw6@gmail.com>
2026-04-01 23:26:01 -0700

    chore: migrato to vcs

---
+++ Scratch/Labuladong/Array/leetcode/editor/en/medium1314MatrixBlockSum.cs
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1314 lang=csharp
+ * @lcpr version=30402
+ *
+ * [1314] Matrix Block Sum
+ */
+
+namespace Scratch.Labuladong.Algorithms.MatrixBlockSum;
+
+// @lc code=start
+public class Solution
+{
+    public int[][] MatrixBlockSum(int[][] mat, int k)
+    {
+        throw new NotImplementedException();
+    }
+}
+// @lc code=end
+/*
+@lcpr case=start
+[[1,2,3],[4,5,6],[7,8,9]]\n1\n
+@lcpr case=end
+
+@lcpr case=start
+[[1,2,3],[4,5,6],[7,8,9]]\n2\n
+@lcpr case=end
+ */
