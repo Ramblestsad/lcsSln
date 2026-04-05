2dacae17a022d0b58b20b92738c5045c746cf5eb
ramblestsad <yilinw6@gmail.com>
2026-04-05 11:07:46 -0700

    build: upgrade deps

---
+++ Scratch/Labuladong/Array/leetcode/editor/en/easy724FindPivotIndex.cs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=724 lang=csharp
+ * @lcpr version=30402
+ *
+ * [724] Find Pivot Index
+ */
+
+namespace Scratch.Labuladong.Algorithms.FindPivotIndex;
+
+// @lc code=start
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        return -1;
+    }
+}
+// @lc code=end
+/*
+@lcpr case=start
+[1,7,3,6,5,6]\n
+@lcpr case=end
+
+@lcpr case=start
+[1,2,3]\n
+@lcpr case=end
+
+@lcpr case=start
+[2,1,-1]\n
+@lcpr case=end
+ */
