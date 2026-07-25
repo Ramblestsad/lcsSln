4b96338dc3c50ccdf6ca0a9f747383361d006f56
yilinw <yilinw6@gmail.com>
2026-07-24 17:11:52 -0700

    feat: implement ACM input handling with Basic and Fast classes

--- Scratch/Utils/stdIO.cs
+++ Scratch/Utils/stdIO.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Scratch.Utils;
 
 public static class StdRead
@@ -74,43 +76,98 @@ public static class StdRead
     }
 }
 
-class FastScanner
+public static class ACMScan
 {
-    private readonly Stream _stream = Console.OpenStandardInput();
-    private readonly byte[] _buffer = new byte[1 << 16];
-    private int _len, _ptr;
-
-    private int ReadByte()
+    /// <summary>
+    /// 普通 ACM 输入：先熟悉 Console.ReadLine + Split 写法。
+    /// </summary>
+    public static class Basic
     {
-        if (_ptr >= _len)
-        {
-            _len = _stream.Read(_buffer, 0, _buffer.Length);
-            _ptr = 0;
-            if (_len == 0) return -1;
-        }
+        public static int ReadInt(TextReader? input = null) =>
+            int.Parse(( input ?? Console.In ).ReadLine()!);
 
-        return _buffer[_ptr++];
+        public static int[] ReadIntArray(TextReader? input = null) =>
+            ( input ?? Console.In ).ReadLine()!
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
     }
 
-    public int NextInt()
+    /// <summary>
+    /// 大量数据输入：用字节缓冲区按空白分隔读取。
+    /// </summary>
+    public sealed class Fast
     {
-        int c;
-        while (( c = ReadByte() ) <= ' ' && c != -1) ;
-        var sign = 1;
-        if (c == '-')
+        private readonly Stream _input;
+        private readonly byte[] _buffer = new byte[1 << 16];
+        private int _length;
+        private int _position;
+
+        public Fast(Stream? input = null)
         {
-            sign = -1;
-            c = ReadByte();
+            _input = input ?? Console.OpenStandardInput();
         }
 
-        var val = 0;
-        while (c > ' ')
+        private int ReadByte()
         {
-            val = val * 10 + ( c - '0' );
-            c = ReadByte();
+            if (_position < _length)
+                return _buffer[_position++];
+
+            _length = _input.Read(_buffer, 0, _buffer.Length);
+            _position = 0;
+            return _length == 0 ? -1 : _buffer[_position++];
         }
 
-        return val * sign;
+        private int NextNonWhitespace()
+        {
+            int c;
+            while (( c = ReadByte() ) <= ' ' && c != -1) { }
+            return c;
+        }
+
+        public string Next()
+        {
+            var c = NextNonWhitespace();
+            if (c == -1)
+                throw new EndOfStreamException();
+
+            var token = new StringBuilder();
+            while (c > ' ')
+            {
+                token.Append((char)c);
+                c = ReadByte();
+            }
+
+            return token.ToString();
+        }
+
+        public int NextInt() => checked((int)NextLong());
+
+        public long NextLong()
+        {
+            var c = NextNonWhitespace();
+            if (c == -1)
+                throw new EndOfStreamException();
+
+            var negative = c == '-';
+            if (c is '-' or '+')
+                c = ReadByte();
+
+            if (c is < '0' or > '9')
+                throw new FormatException("Expected an integer.");
+
+            long value = 0;
+            while (c > ' ')
+            {
+                if (c is < '0' or > '9')
+                    throw new FormatException("Expected an integer.");
+
+                value = checked(value * 10 - ( c - '0' ));
+                c = ReadByte();
+            }
+
+            return negative ? value : checked(-value);
+        }
     }
 }
 
